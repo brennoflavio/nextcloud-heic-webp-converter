68ac91aec465adab014b5285cc7081037370e4ea
brennoflavio <contato@brennoflavio.com.br>
2023-10-03 02:49:22 -0300

    first commit :rocket:

---
+++ README.md
@@ -0,0 +1,31 @@
+# Nextcloud HEIC converter
+ 
+## Why?
+
+I could not get HEIC previews working correcly when using Nextcloud on FreeBSD.
+So I made this script to convert them to jpg instead, which works fine for previews.
+
+### How?
+
+This Python script uses the WebDAV protocol to convert .heic files in a given Nextcloud folder to .jpg.
+The script works by finding all .heic files in the specified folder, downloading each file, converting it to .jpg,
+then re-uploading the new jpg file back to the same location in the Nextcloud server. The original .heic file is
+subsequently deleted from the server.
+
+#### Usage
+Set up the environment variables in a `.env` file (see from `.env.example`):
+
+```
+NEXTCLOUD_DAV_ENDPOINT: Your Nextcloud WebDAV endpoint.
+NEXTCLOUD_USERNAME: Your Nextcloud username.
+NEXTCLOUD_PASSWORD: Your Nextcloud password.
+NEXTCLOUD_FOLDER: The path of the Nextcloud folder you wish to convert files in.
+
+```
+
+You can run the script with Poetry:
+`poetry run nextcloud_heic_converter/main.py`
+
+#### Requirements
+
+You need `imagemagick` installed as this script relies on its `convert` command.
---
+++ nextcloud_heic_converter/main.py
@@ -0,0 +1,86 @@
+import logging
+from webdav3.client import Client
+from tempfile import NamedTemporaryFile
+from dotenv import load_dotenv
+import os
+from urllib.parse import urlparse
+from tempfile import NamedTemporaryFile
+import subprocess
+
+load_dotenv()
+logging.basicConfig(level=logging.INFO)
+
+def get_client():
+    options = {
+        "webdav_hostname": os.getenv("NEXTCLOUD_DAV_ENDPOINT"),
+        "webdav_login": os.getenv("NEXTCLOUD_USERNAME"),
+        "webdav_password": os.getenv("NEXTCLOUD_PASSWORD"),
+    }
+    client = Client(options)
+    return client
+
+def sanitize_path(path):
+    path_to_strip = urlparse(os.getenv("NEXTCLOUD_DAV_ENDPOINT")).path
+    return "/" + path.replace(path_to_strip, "")
+
+
+def expand_directory(folder, client):
+    files = []
+    file_list = client.list(folder, get_info=True)
+
+    for file in file_list:
+        file["path"] = sanitize_path(file["path"])
+        if file["path"] == folder:
+            continue
+        if file.get("isdir"):
+            files.extend(expand_directory(file["path"], client))
+        else:
+            files.append(file)
+
+    return [x.get("path") for x in file_list if not x.get("isdir")]
+
+def download_file(remote_path, local_path, client):
+    return client.download_sync(remote_path=remote_path, local_path=local_path)
+
+def upload_file(remote_path, local_path, client):
+    return client.upload_sync(remote_path=remote_path, local_path=local_path)
+
+def delete_file(remote_path, client):
+    return client.clean(remote_path)
+
+def change_file_extension(file_path, new_extension):
+    head, tail = os.path.split(file_path)
+    filename, _ = os.path.splitext(tail)
+    return os.path.join(head, f"{filename}.{new_extension}")
+
+def get_file_extension(file_path):
+    _, tail = os.path.split(file_path)
+    _, ext = os.path.splitext(tail)
+    return ext[1:]
+
+def convert_file(heic_file):
+    logging.info(f"Converting {heic_file}")
+    subprocess.run(["heic2jpg", "-s", heic_file, "--keep"])
+    return change_file_extension(heic_file, "jpg")
+
+def main():
+    client = get_client()
+    assert client
+
+    folder = os.getenv("NEXTCLOUD_FOLDER")
+    assert folder
+
+    files = [x for x in expand_directory(folder, client) if get_file_extension(x) == "heic"]
+    logging.info(f"Going to do {len(files)}")
+
+    for f in files:
+        with NamedTemporaryFile("w+", suffix=".heic") as temp_file:
+            download_file(f, temp_file.name, client)
+            new_file = convert_file(temp_file.name)
+            new_remote_path = change_file_extension(f, "jpg")
+            logging.info(f"Converting {f} to {new_remote_path}")
+            upload_file(new_remote_path, temp_file.name, client)
+            delete_file(f, client)
+
+if __name__ == "__main__":
+    main()
